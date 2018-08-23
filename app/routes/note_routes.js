4bfb7e99352178652221c731c26d1437f3165a64
alsidneio <alsidneio@gmail.com>
2018-08-23 01:08:23 -0700

    saving files for later

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app,db){
     app.post('/notes', (req,res) => {
         //youll create your note here 
-        console.log()
+        console.log(req.body)
         res.send('Hello')
     });
 };
